3b39786b39d066ae5ea044d2fb1d7859b8f74f2c
taylorkraft <taylorkraft16@gmail.com>
2020-06-05 20:10:40 -0600

    base url

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const BASE_URL = "127.0.0.1:3000"
+const BASE_URL = "http://localhost:3000/"
 const SENSEI_URL = `${BASE_URL}/senseis`
 const NINJA_URL = `${BASE_URL}/ninjas`
 const main = document.querySelector("main")
